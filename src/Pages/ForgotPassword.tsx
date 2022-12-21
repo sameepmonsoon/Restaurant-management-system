2d25d0d8ab7c079a70cd73f3015fe61b66629a0a
unknown <shibaaadhikari0@gmail.com>
2022-12-21 15:36:51 +0545

    forgot password

--- src/Pages/ForgotPassword.tsx
+++ src/Pages/ForgotPassword.tsx
@@ -7,17 +7,23 @@ import { Caption } from "./Forgot.Style";
 import { FormDiv } from "./Forgot.Style";
 import { TextField } from "../Components/TextField";
 import { Button } from "./Forgot.Style";
+import { FormError } from "./Forgot.Style";
+import { Title } from "./Forgot.Style";
 const ForgotPassword = () => {
-  let schema= yup.object().shape({
-    newpassword:yup.string().min(6).required("required"),
-    confirmnewpassword:yup.string().min(6).required("required"),
+  let schema = yup.object().shape({
+    newpassword: yup.string().min(6).required("required"),
+    confirmnewpassword: yup
+      .string()
+      .required()
+      .oneOf([yup.ref("newpassword"), null], "Password must match"),
   });
-  const {values,handleSubmit,handleChange,errors,touched}=useFormik({
-    initialValues:{newpassword:"",confirmnewpassword:""},
-    onSubmit:(values)=>{
-        
-    }
-  })
+  const { values, handleSubmit, handleChange, errors, touched } = useFormik({
+    initialValues: { newpassword: "", confirmnewpassword: "" },
+    onSubmit: (values) => {
+      console.log("values", values);
+    },
+    validationSchema: schema,
+  });
 
   return (
     <>
@@ -27,13 +33,35 @@ const ForgotPassword = () => {
           <Caption>Welcome to Kpop Dashboard</Caption>
         </Image>
         <FormDiv>
-          <form>
-            <TextField name="newpassword" label="New Password"></TextField>
-            <TextField name="confirmnewpassword" label=" Confirm New Password"></TextField>
+          {/* <Title>
+            <p> Forgot Password</p>
+          </Title> */}
+
+{/* <p>Forgot Password</p> */}
+          <form onSubmit={handleSubmit}>
+            <TextField
+              onChange={handleChange}
+              name="newpassword"
+              label="New Password"
+              value={values.newpassword}
+            ></TextField>
+            {errors.newpassword && touched.newpassword ? (
+              <FormError>{errors.newpassword}</FormError>
+            ) : null}
+
+            <TextField
+              onChange={handleChange}
+              name="confirmnewpassword"
+              label=" Confirm New Password"
+              value={values.confirmnewpassword}
+            ></TextField>
+            {errors.confirmnewpassword && touched.confirmnewpassword ? (
+              <FormError>{errors.confirmnewpassword}</FormError>
+            ) : null}
+
             <Button type="submit">Sign in</Button>
           </form>
         </FormDiv>
-     
       </MainDiv>
     </>
   );
