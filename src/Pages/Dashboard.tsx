9cf762fc6d8814a4893991ed08274bd206acee80
rajiv dahak <rajivdahal2@gmail.com>
2022-12-26 11:15:00 +0545

    major changes

--- src/Pages/Dashboard.tsx
+++ src/Pages/Dashboard.tsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import DashboardLayout from '../Layout/DashboardLayout'
+import Navbar from '../PageComponent/Dashboard/Navbar/Navbar'
 
 export default function Dashboard() {
   return (
-    <div>Dashboard</div>
-  )
+  <DashboardLayout
+    renderActions={
+      // Please make a button as a component which can be reusable in future  inside a Component directory which accepts Icon,Title and and OnClick props
+      // And place it over here by replacing below code 
+        <div>Add purchase button</div>
+    }
+    renderFilters={
+      <div>Filters</div>
+    }
+  >
+      <h1>Dashboard Homepage</h1>
+  </DashboardLayout>
+)
 }
