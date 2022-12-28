fa7f96e9ba8fe5c85a5465c186e86b1317b29562
rajiv dahak <rajivdahal2@gmail.com>
2022-12-28 15:36:08 +0545

    login successfult

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -14,6 +14,7 @@ import { useEffect } from "react";
 import axios from "axios"
 import { HTTPMethods } from "../Utils/HTTPMock";
 import { useNavigationAfterTokenCheck } from "../Hooks/useNavigateToLogin";
+import { ResponseType } from "../Types/Utils/ResponseTypes";
 export function Login() {
   const redirect=useNavigationAfterTokenCheck()
   const navigate=useNavigate()
@@ -30,14 +31,16 @@ export function Login() {
       // NOTE : Currently we are using our token this token will be saved in localstorage after login response from backend
       // localStorage.setItem("token","this is token")
       // navigate("/home")
-      HTTPMethods.post("/login",values)
-      .then(function(resp){
-          console.log("response is",resp)
-        // NOTE : Save token localStorage.setItem("token",resp.token)
-        // console.log("response is",resp)
-      // Navigate to "/home"
+      HTTPMethods.post("/auth/login",values)
+      .then(async function(resp:any){
+      const {payload}=resp.data 
+      const {token}=payload
+      await localStorage.setItem("token",token)
+      // Success Message
+      navigate("/home")
       })
       .catch(function(err){
+        // Error Message
         console.log("error is",err)
       })  
       },
