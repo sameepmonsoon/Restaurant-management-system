1122d1ef8186647139a1a9d88eeb289db5e0620b
Sameep Rana <sameep.monsoon@gmail.com>
2022-12-29 13:55:08 +0545

    Dashboard Table

--- src/PageComponent/Dashboard/Table/Table.tsx
+++ src/PageComponent/Dashboard/Table/Table.tsx
@@ -1,19 +1,42 @@
 import React from 'react'
 import { MainDiv } from '../../../Components/TextField.Style'
-import { MainTableDiv, TableBody, TableData, TableHeader,TableHeadData, TableRow } from './Table.styles'
+import {HiChevronDown} from 'react-icons/hi'
+import {HiDotsVertical} from 'react-icons/hi'
+import { MainTableDiv, TableBody, TableData, TableHeader,TableHeadData, TableRow,TableDataForDate } from './Table.styles'
 
 const Table = () => {
   return (
   <>
   <MainTableDiv>
-    <TableHeader><TableHeadData> <input type="checkbox" name="" id="" /> Data</TableHeadData>
-    <TableHeadData>Data</TableHeadData><TableHeadData>Data</TableHeadData>
-    <TableHeadData>Data</TableHeadData><TableHeadData>Data</TableHeadData><TableHeadData>Data</TableHeadData>
-    <TableHeadData>Data</TableHeadData> 
+    <TableHeader><TableHeadData> <input type="checkbox" name="" id="" />id <HiChevronDown/> </TableHeadData>
+    <TableHeadData>product<HiChevronDown/></TableHeadData>
+    <TableHeadData>quantity<HiChevronDown/></TableHeadData>
+    <TableHeadData>per price<HiChevronDown/></TableHeadData>
+    <TableHeadData>net   price<HiChevronDown/></TableHeadData>
+    <TableHeadData>status<HiChevronDown/></TableHeadData>
+    <TableHeadData>date<HiChevronDown/></TableHeadData> 
     </TableHeader>
     <TableBody>
         <TableRow>
-            <TableData>Row data</TableData>
+            <TableData>01</TableData>
+        
+            <TableData>Macheral Fish</TableData>
+        
+            <TableData>5 Packets</TableData>
+            <TableData>1000</TableData>
+        
+            <TableData>50000</TableData>
+        
+            <TableData>
+                status here</TableData>
+            <TableData>date here
+               <TableDataForDate>                <HiDotsVertical size={20}/>
+               </TableDataForDate>
+
+           </TableData>
+        </TableRow>
+        <TableRow>
+            <TableData>No.</TableData>
         
             <TableData>Row data</TableData>
         
@@ -23,7 +46,11 @@ const Table = () => {
             <TableData>Row data</TableData>
         
             <TableData>Row data</TableData>
-            <TableData>Row data</TableData>
+            <TableData>Row data
+               <TableDataForDate>                <HiDotsVertical size={20}/>
+               </TableDataForDate>
+
+           </TableData>
         </TableRow>
     </TableBody>
   </MainTableDiv>
