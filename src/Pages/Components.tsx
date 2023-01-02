886a8a933e4fe1f43bf31ecb9aefff44181ff65f
SabinaRe <sabinarana20devin@gmail.com>
2023-01-02 11:33:43 +0545

    sider conflict resolved

--- src/Pages/Components.tsx
+++ src/Pages/Components.tsx
@@ -4,13 +4,16 @@ import { TextField } from '../Components/TextField'
 import * as yup from 'yup';
 import InventoryCard from '../Components/InventoryCard/InventoryCard';
 import { MdAdd, MdOutlineShoppingCart } from 'react-icons/md';
-
-
-import {
-  DashboardComponents
-} from './Components.styles'
+import {DashboardComponents} from './Components.styles'
 import Navbar from '../PageComponent/Dashboard/Navbar/Navbar';
 import ActionButton from '../Components/ActionButton/ActionButton';
+import {
+  Children,
+  ChildrenItem,
+} from "../Components/SiderButton/SiderButton.Style";
+import SiderButton from '../Components/SiderButton/SiderButton';
+import { FaBeer } from 'react-icons/fa';
+import { RiArrowDropDownLine } from 'react-icons/ri';
 
 export default function Components() {
     let schema = yup.object().shape({
@@ -32,22 +35,33 @@ function onSumbit(e:any){
 }
   return (
     <>
-    <div>Components</div>
-   
-    <DashboardComponents>
+      <div>Components</div>
+      
+      <DashboardComponents>
+          <TextField
+            name="name"
+            onChange={handleChange}
+            error={errors.name}
+            prefix={"name"}
+            suffix={"suffix"}
+          />
 
-    
-      <TextField name="name"  onChange={handleChange} error={errors.name} prefix={"name"} suffix={"suffix"} />
+        <InventoryCard  title={"purchase"} icon={<MdOutlineShoppingCart size={30}/>} amount={"10,000"} cardType="purchase" active={true}/>
 
-      <InventoryCard  title={"purchase"} icon={<MdOutlineShoppingCart size={30}/>} amount={"10,000"} cardType="purchase" active={true}/>
-
-      <ActionButton icon={<MdAdd fontSize="1.4em"/>} label={"ADD PURCHASE"} onClick={(e: React.MouseEvent<HTMLElement>) => console.log("Clicked")} />
-    
-    
-    </DashboardComponents>
-   
-
-
+          <ActionButton
+            icon={<MdAdd fontSize="1.4em" />}
+            label={"ADD PURCHASE"}
+            onClick={(e: React.MouseEvent<HTMLElement>) => console.log("Clicked")}
+          />
+          <SiderButton titleIcon={<FaBeer />} title="Icon" childrenIcon={<RiArrowDropDownLine/>}>
+            <Children>
+              <ChildrenItem>Purchase</ChildrenItem>
+              <ChildrenItem>Sales</ChildrenItem>
+              <ChildrenItem>Stock</ChildrenItem>
+            </Children>
+          </SiderButton>
+      
+      </DashboardComponents>
     </>
   )
 }
