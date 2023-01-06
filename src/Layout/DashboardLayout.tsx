c9fb30cade965cfd2ba19f08837a7f56ec4cee29
SabinaRe <sabinarana20devin@gmail.com>
2023-01-06 11:30:07 +0545

    upated css for dashboardlayout

--- src/Layout/DashboardLayout.tsx
+++ src/Layout/DashboardLayout.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import {MdOutlineShoppingCart} from 'react-icons/md'
 import {BsTag} from "react-icons/bs"
+import {CiBoxes} from "react-icons/ci"
 import { toast } from 'react-toastify'
 import InventoryCard from '../Components/InventoryCard/InventoryCard'
 import Navbar from '../PageComponent/Dashboard/Navbar/Navbar'
@@ -72,7 +73,7 @@ export default function DashboardLayout({children,renderActions,renderFilters}:{
 
             <InventoryCard  title={"Purchase"} icon={<MdOutlineShoppingCart size={30}/>} amount={`Rs. ${purchases?.totalpurchase}`} cardType="purchase" active={true} />
             <InventoryCard  title={"Sales"} icon={<BsTag size={30}/>} amount={`Rs. ${purchases?.total_sales}`} cardType="sales" active={true}/>
-            <InventoryCard  title={"Stocks"} icon={<MdOutlineShoppingCart size={30}/>} amount={`${purchases?.totalpurchase} products`} cardType="stock" active={true}/>
+            <InventoryCard  title={"Stocks"} icon={<CiBoxes size={35}/>} amount={`${purchases?.totalpurchase} products`} cardType="stock" active={true}/>
               {renderActions}
             </InventoryCardContainerDiv>
             {renderFilters}
