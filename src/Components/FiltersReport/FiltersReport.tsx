55a3857f47e46c0d289a30bec9eb07dc52b55507
Sameep Rana <sameep.monsoon@gmail.com>
2023-02-07 14:22:29 +0545

    report fgeneration for filter report  remaining--not complete

--- src/Components/FiltersReport/FiltersReport.tsx
+++ src/Components/FiltersReport/FiltersReport.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { AiOutlinePrinter } from "react-icons/ai";
-import { TextField } from "../TextField";
+// import { TextField } from "../TextField";
+import { Dayjs } from "dayjs";
 import { RxDividerVertical } from "react-icons/rx";
 import DashboardButton from "../DashboardButton/DashboardButton";
 import { useFilterStore } from "../../store/filtered";
@@ -20,6 +21,12 @@ import {
   ReportDaily,
 } from "./FiltersReport.styles";
 import { MyContext } from "../../Pages/Reports";
+import TextField from "@mui/material/TextField";
+import { LocalizationProvider } from "@mui/x-date-pickers-pro";
+import { AdapterDayjs } from "@mui/x-date-pickers-pro/AdapterDayjs";
+import { StaticDateRangePicker } from "@mui/x-date-pickers-pro/StaticDateRangePicker";
+import { DateRange } from "@mui/x-date-pickers-pro/DateRangePicker";
+import Box from "@mui/material/Box";
 
 const FiltersReport = () => {
   const { searchTerm, setSearchTerm } = useFilterStore((state: any) => ({
@@ -27,44 +34,98 @@ const FiltersReport = () => {
     setSearchTerm: state.setSearchTerm,
   }));
 
-  const [value, setValue] = useState([null, null]);
+  const [value, setValue] = React.useState<DateRange<Dayjs>>([null, null]);
   return (
-    <>
-      <ReportFilterMainDiv>
-        <ReportFilterInnerDiv>
-          {
-            console.log("value inside consumer",value)
+    <MyContext.Consumer>
+      {(value) => {
+        function changeValue(e: any) {
+          if (e.target.value === "daily") {
+            console.log("inside change value", value, e.target.value);
+            return value?.setDateAndTime({
+              date1: null,
+              date2: null,
+              isMonthly: false,
+              isWeekly: false,
+              isDaily: true,
+            });
+            // value.isMonthly=false
+            // value.isWeekly=false
+            // return value.isDaily=true// change to daily
           }
-          <ReportFilterType>
-            <p> Report Type </p>
-            <select name="select" id="" onChange={changeValue}>
-              <option value="daily">Daily</option>
-              <option value="weekly">Weekly</option>
-              <option value="monthly">Monthly</option>
-            </select>
-          </ReportFilterType>
-          <ReportFilterTextDate>
-            <div>start date</div>
-            <HiArrowLongDown />
-            <div>end date</div>
-          </ReportFilterTextDate>
-        </ReportFilterInnerDiv>
-        <ReportFilterDateBox>
-          <TextField type="date" label="" />
-          <DashboardButton
-            icon={""}
-            title={"GENERATE REPORT"}
-            location={location.pathname === "/report"}
-            disableTransition={true}
-          />
-        </ReportFilterDateBox>
-        <ReportFilterPrintIcon>
-          <AiOutlinePrinter size={35} />
-        </ReportFilterPrintIcon>
-      </ReportFilterMainDiv>}
-      }
-
-      </MyContext.Consumer>
+          if (e.target.value === "monthly") {
+            return value?.setDateAndTime({
+              date1: null,
+              date2: null,
+              isMonthly: true,
+              isWeekly: false,
+              isDaily: false,
+            });
+          }
+          if (e.target.value === "weekly") {
+            return value?.setDateAndTime({
+              date1: null,
+              date2: null,
+              isMonthly: false,
+              isWeekly: true,
+              isDaily: false,
+            });
+          }
+        }
+        return (
+          <ReportFilterMainDiv>
+            {/* @ts-ignore */}
+            <ReportFilterInnerDiv>
+              {console.log("value inside consumer", value)}
+              <ReportFilterType>
+                <p> Report Type </p>
+                <select name="select" id="" onChange={changeValue}>
+                  <option value="daily">Daily</option>
+                  <option value="weekly">Weekly</option>
+                  <option value="monthly">Monthly</option>
+                </select>
+              </ReportFilterType>
+              <ReportFilterTextDate>
+                {searchTerm === "weekly" || searchTerm === "monthly" ? (
+                  <>
+                    <ReportWeekly>
+                      <ReportWeeklyDiv>start date</ReportWeeklyDiv>
+                      <RxDividerVertical />
+                      <ReportWeeklyDiv>end date</ReportWeeklyDiv>
+                    </ReportWeekly>
+                  </>
+                ) : (
+                  <>
+                    <ReportDaily>
+                      <FiArrowLeftCircle size={25} />
+                      single date
+                      <FiArrowRightCircle size={25} />
+                    </ReportDaily>
+                  </>
+                )}
+              </ReportFilterTextDate>
+            </ReportFilterInnerDiv>
+            <DateButtonBox>
+              {searchTerm === "weekly" || searchTerm === "monthly" ? (
+                <></>
+              ) : (
+                <>
+                  <ReportFilterDateBox></ReportFilterDateBox>
+                </>
+              )}
+              <DashboardButton
+                icon={""}
+                title={"Generate Report"}
+                location={location.pathname.includes("/reports")}
+                disableTransition={true}
+              />
+            </DateButtonBox>
+            <ReportFilterPrintIcon>
+              <AiOutlinePrinter size={35} />
+            </ReportFilterPrintIcon>
+          </ReportFilterMainDiv>
+        );
+      }}
+    </MyContext.Consumer>
   );
 };
 
